4e6bba5e3df8346de7aa4ba5905bc18c30de952a
Mark Croxton <mcroxton@hallmark-design.co.uk>
2021-05-17 12:01:49 +0100

    Actually enable JIT mode (ya dufus)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,11 @@
 module.exports = {
-    purge: {
-        mode: 'jit', // 'all' for traditional Tailwind
-        content: [
-            './web/**/*.html',
-            './templates/**/*.twig',
-            './src/**/*.{js,jsx,ts,tsx,vue}',
-            './src/**/*.{css,scss}',
-        ],
-        options: {
-            safelist: ['show', 'collapse', 'collapsing', 'collapsed'],
-            keyframes: true,
-            fontFace: true,
-        },
-    },
+    mode: 'jit',
+    purge: [
+        './web/**/*.html',
+        './templates/**/*.twig',
+        './src/**/*.{js,jsx,ts,tsx,vue}',
+        './src/**/*.{css,scss}',
+    ],
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {},
